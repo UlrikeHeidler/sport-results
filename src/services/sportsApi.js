59418ddf55b59fb57c3547f6252488263af5f375
Ulrike Heidler <ulrike.heidler.biz@gmail.com>
2025-11-05 19:57:15 -0500

    Fixing the break/halftime/intermission label

--- src/services/sportsApi.js
+++ src/services/sportsApi.js
@@ -376,7 +376,7 @@ export const formatGameTime = (date, status = {}, league = '') => {
     
     
     if (isGameInBreak(status)) {
-      return getLiveStateLabel(status);
+      return getLiveStateLabel(status.type);
     }
 
     if (isGameOngoing(status)) {
